6e68698492fee95d7a5a60df2f0236b91037b61f
Justin Couch <justincouchdesign@gmail.com>
2019-05-07 15:01:08 -0400

    mapping

--- makr_sign.py
+++ makr_sign.py
@@ -45,11 +45,11 @@ PIXEL_MAPPING = [
     (7,0),
     (8,0),
     (9,0),
-    (10,0), ## 10 
+    (10,0), ## 10
     (11,0),
     (12,0),
     (13,0),
-    (14,0), ## 14, top right corner 
+    (14,0), ## 14, top right corner
     (14,1),
     (14,2),
     (14,3),
@@ -90,7 +90,73 @@ PIXEL_MAPPING = [
     (0,4),
     (0,3),
     (0,2),
-    (0,1)
+    (0,1),  ## 55 end of square
+
+    (2,2), ## 56 top left of M
+    (2,3),
+    (2,4),
+    (2,5),
+    (2,6), ## 60 bot left of M
+    (3,6),
+    (4,6),
+    (5,6),
+    (5,5),
+    (4,4),
+    (3,3),
+    (5,3),
+    (6,2),
+    (6,3),
+    (6,4),
+    (6,5),
+    (6,6), ## 72 bot right of M
+
+    (8,6), ## 73 bot left of A
+    (8,5), ## 74, 8.5, 5
+    (9,4),
+    (9,3), ## 76, 9.5, 3
+    (10,2), ## top of A
+    (10,3), ## 78, 10.5,3
+    (11,4),
+    (11,5), ## 80, 11.5,5
+    (12,6), ## bot right of A
+    (11,6),
+    (10,6),
+    (9,6), ## 84, end of A
+
+    (8,8), ## 85, top left of R
+    (9,8),
+    (10,8),
+    (11,8),
+    (12,8), ## 89 top rt of R
+    (12,9),
+    (12,10),
+    (11,10),
+    (12,12), ## 93 bot rt of R
+    (11,11),
+    (10,10),
+    (9,9),
+    (8,9),
+    (8,10),
+    (8,11),
+    (8,12), ## 100 bot left of R
+
+    (6,12), ## 101 bot rt of K
+    (5,12),
+    (4,12),
+    (3,12),
+    (2,12), ## 105 bot left of K
+    (2,11),
+    (2,10),
+    (2,9),
+    (2,8), ## 109 top left of K
+    (3,8),
+    (4,8),
+    (5,8),
+    (6,8), ## 113 top rt of K
+    (5,7),
+    (4,6),
+    (3,5),
+    (5,5)  ## 117 end
     ]
 
 
@@ -98,7 +164,7 @@ def convert_int_to_rgb(col):
     b = (col & BLUE_MASK) >> 16
     r = (col & RED_MASK)
     g = (col & GREEN_MASK) >> 8
-    return (r, g, b) if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0) 
+    return (r, g, b) if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0)
 
 
 
@@ -115,7 +181,7 @@ def randomize_IMAGE():
     for i in range(NUM_ROWS):
         for j in range(NUM_ROWS):
             IMAGE[i][j] = numpy.random.randint(0,16777215)
-    
+
 while True:
     randomize_IMAGE()
     set_pixels_from_IMAGE()
