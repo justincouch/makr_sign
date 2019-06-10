79c4afab2b84c1d0b60ffbf804b4c1a3a1382575
Justin Couch <justincouchdesign@gmail.com>
2019-06-10 14:47:27 +0000

    test board

--- makr_sign.py
+++ makr_sign.py
@@ -8,7 +8,7 @@ import math
 import PIL.Image
 from tkinter import *
 
-USE_VIZ_CANVAS =False
+USE_VIZ_CANVAS = False
 COLOR_MODE = "RAINBOW" #"LOGO"
 ANIM_MODE = "PERLIN" #"PERLIN" "RIPPLE"
 
@@ -20,15 +20,15 @@ im_array = numpy.array(im)
 #print(im_vals)
 
 pixel_pin = board.D18
-num_pixels = 118
+num_pixels = 19
 ripple_ctr = 0
-ORDER = neopixel.GRB
+ORDER = neopixel.RGB
 CHASE_INTERVAL = 0.05
 RAINBOW_INTERVAL = 0.01
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1.0, auto_write=False, pixel_order=ORDER)
 
-NUM_ROWS = 15
+NUM_ROWS = 5
 
 ##IMAGE = numpy.zeros( (NUM_ROWS,NUM_ROWS) )
 IMAGE = [[ numpy.random.randint(0,16777215) for x in range(NUM_ROWS) ] for y in range(NUM_ROWS) ]
@@ -67,123 +67,20 @@ PIXEL_MAPPING = [
     (2,0), ## 2
     (3,0),
     (4,0),
-    (5,0),
-    (6,0),
-    (7,0),
-    (8,0),
-    (9,0),
-    (10,0), ## 10
-    (11,0),
-    (12,0),
-    (13,0),
-    (14,0), ## 14, top right corner
-    (14,1),
-    (14,2),
-    (14,3),
-    (14,4),
-    (14,5),
-    (14,6),
-    (14,7),
-    (14,8),
-    (14,9),
-    (14,10),
-    (14,11),
-    (14,12),
-    (14,13),
-    (14,14), ## 28 bottom right corner
-    (13,14),
-    (12,14),
-    (11,14),
-    (10,14),
-    (9,14),
-    (8,14),
-    (7,14),
-    (6,14),
-    (5,14),
-    (4,14),
-    (3,14),
-    (2,14),
-    (1,14),
-    (0,14), ## 42 bottom left
-    (0,13),
-    (0,12),
-    (0,11),
-    (0,10),
-    (0,9),
-    (0,8),
-    (0,7),
-    (0,6),
-    (0,5),
-    (0,4),
-    (0,3),
-    (0,2),
-    (0,1),  ## 55 end of square
-
-    (2,2), ## 56 top left of M
-    (2,3),
-    (2,4),
-    (2,5),
-    (2,6), ## 60 bot left of M
-    (3,6),
-    (4,6),
-    (5,6),
-    (5,5),
+    (4,1),
+    (4,2),
+    (4,3),
     (4,4),
-    (3,3),
-    (5,3),
-    (6,2),
-    (6,3),
-    (6,4),
-    (6,5),
-    (6,6), ## 72 bot right of M
-
-    (8,6), ## 73 bot left of A
-    (8,5), ## 74, 8.5, 5
-    (9,4),
-    (9,3), ## 76, 9.5, 3
-    (10,2), ## top of A
-    (10,3), ## 78, 10.5,3
-    (11,4),
-    (11,5), ## 80, 11.5,5
-    (12,6), ## bot right of A
-    (11,6),
-    (10,6),
-    (9,6), ## 84, end of A
-
-    (8,8), ## 85, top left of R
-    (9,8),
-    (10,8),
-    (11,8),
-    (12,8), ## 89 top rt of R
-    (12,9),
-    (12,10),
-    (11,10),
-    (12,12), ## 93 bot rt of R
-    (11,11),
-    (10,10),
-    (9,9),
-    (8,9),
-    (8,10),
-    (8,11),
-    (8,12), ## 100 bot left of R
-
-    (6,12), ## 101 bot rt of K
-    (5,12),
-    (4,12),
-    (3,12),
-    (2,12), ## 105 bot left of K
-    (2,11),
-    (2,10),
-    (2,9),
-    (2,8), ## 109 top left of K
-    (3,8),
-    (4,8),
-    (5,8),
-    (6,8), ## 113 top rt of K
-    (5,9),
-    (4,10),
-    (3,11),
-    (5,11)  ## 117 end
+    (3,4), ## 10
+    (2,4),
+    (1,4),
+    (0,4),
+    (0,3), ## 14, top right corner
+    (0,2),
+    (0,1),
+    (1,1),
+    (2,2),
+    (3,3)
     ]
 
 
@@ -437,7 +334,7 @@ def vis_ripple():
 zinc = 0
 
 ##viz_image(im_array)
-set_pixels_from_IMAGE()
+##set_pixels_from_IMAGE()
 
 while True:
 ##    time.sleep(0.5)
@@ -450,6 +347,7 @@ while True:
 ##    viz_perlin_logo(zinc)
 ##    set_pixels_from_IMAGE()
 ##    zinc += 0.02
+    
     if ANIM_MODE == "PERLIN":
         vis_perlin_lib(zinc)
         set_pixels_from_IMAGE()
